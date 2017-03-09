4f6ba4f4e662b441b10cdd22de52beadb6b59628
Bobby Smith <bobby.smith@gmail.com>
2017-03-09 11:53:04 -0500

    Test Case Results for Demo a spreadsheet test case

---
+++ gauge/tests/Demo a spreadsheet test case.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("Open spreadsheet - should - Spreadsheet opened", function() {
+  assert.equal(true, "Open spreadsheet - should - Spreadsheet opened");
+});
