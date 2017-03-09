788144536a1dab1597e77acb5da6a1e1d1df0e5e
Bobby Smith <bobby.smith@gmail.com>
2017-03-09 09:42:42 -0500

    Test Case Results for Quickly Added Test Case

---
+++ gauge/tests/Quickly Added Test Case.js
@@ -0,0 +1,10 @@
+
+/* globals gauge*/
+
+"use strict";
+
+var assert = require("assert");
+
+gauge.step("Set up the integration - should - Integration Completed", function() {
+  assert.equal(true, "Set up the integration - should - Integration Completed");
+});
