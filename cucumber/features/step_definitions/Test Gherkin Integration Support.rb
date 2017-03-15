c3b1ee53b7908d622f9c4185970688bf153a49f5
Bobby Smith <bobby.smith@gmail.com>
2017-03-15 13:18:20 -0400

    Test Case Results for Test Gherkin Integration Support

---
+++ cucumber/features/step_definitions/Test Gherkin Integration Support.rb
@@ -0,0 +1,13 @@
+#encoding: utf-8
+
+Given /^Gherkin Ruby Must be installed in the project$/ do 
+  # enter setup code here
+end
+
+When /^Log In to the UI with correct credentials$/ do | expectedResult |
+  # Actual Test Execution Code
+end
+
+Then /^Login works$/ do | actualResult |
+  expect actualResult.to_s == "Login works"
+end
