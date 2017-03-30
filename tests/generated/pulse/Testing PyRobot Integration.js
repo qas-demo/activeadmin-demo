e2be4a9b519383e70e34547b948537dd40cd8170
Bobby Smith <bobby.smith@gmail.com>
2017-03-30 10:08:41 -0400

    Test Case Results for Testing PyRobot Integration

---
+++ tests/generated/pulse/Testing PyRobot Integration.js
@@ -0,0 +1,28 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Testing PyRobot Integration
+* 
+* This is an interesting description
+*/
+
+/*
+* Preconditions: Some precondition
+* Should write a setup step here
+*/
+
+describe("Testing PyRobot Integration", function(){
+  it("should Some test step", function() {
+    assert("Some Expected Result" === "");
+  });
+});
+
+/*
+gauge.step("", function() {
+  assert.equal("Some Expected Result", "");
+});
+*/
