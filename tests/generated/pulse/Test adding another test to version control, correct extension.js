5f5e3a1c35995232b7a464fe3fedfe9dbb7c8d82
Bobby Smith <bobby.smith@gmail.com>
2017-03-30 07:56:16 -0400

    Test Case Results for Test adding another test to version control, correct extension

---
+++ tests/generated/pulse/Test adding another test to version control, correct extension.js
@@ -0,0 +1,28 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Test adding another test to version control, correct extension
+* 
+* A description
+*/
+
+/*
+* Preconditions: A precondition
+* Should write a setup step here
+*/
+
+describe("Test adding another test to version control, correct extension", function(){
+  it("should A step", function() {
+    assert("A result" === "My Actual Result");
+  });
+});
+
+/*
+gauge.step("", function() {
+  assert.equal("A result", "My Actual Result");
+});
+*/
