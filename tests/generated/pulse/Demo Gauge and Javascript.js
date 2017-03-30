a5c77b51bc477b0e78ec070051bedff8068fd60e
Bobby Smith <bobby.smith@gmail.com>
2017-03-30 09:26:39 -0400

    Test Case Results for Demo Gauge and Javascript

---
+++ tests/generated/pulse/Demo Gauge and Javascript.js
@@ -0,0 +1,28 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Demo Gauge and Javascript
+* 
+* This is meant to demo gauge and javascript
+*/
+
+/*
+* Preconditions: The gauge binary must be installed
+* Should write a setup step here
+*/
+
+describe("Demo Gauge and Javascript", function(){
+  it("should Execute gauge test runner", function() {
+    assert("Get some test results back" === "");
+  });
+});
+
+/*
+gauge.step("", function() {
+  assert.equal("Get some test results back", "");
+});
+*/
