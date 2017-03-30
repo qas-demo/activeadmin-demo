8b0d8c1c4db28faffe9eccf9378d7222728b003a
Bobby Smith <bobby.smith@gmail.com>
2017-03-30 09:37:18 -0400

    Test Case Results for Demo 123

---
+++ tests/generated/pulse/Demo 123.js
@@ -0,0 +1,28 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Demo 123
+* 
+* This is a demo test case
+*/
+
+/*
+* Preconditions: Must have maven installed locally
+* Should write a setup step here
+*/
+
+describe("Demo 123", function(){
+  it("should Execute Gherkin Test Cases", function() {
+    assert("Test Cases Execute" === "");
+  });
+});
+
+/*
+gauge.step("", function() {
+  assert.equal("Test Cases Execute", "");
+});
+*/
