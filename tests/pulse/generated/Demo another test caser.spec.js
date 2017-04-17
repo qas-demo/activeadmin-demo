4659ea3dfb38cda7912c2e0c70ce5f65cc1321a6
Bobby Smith <bobby.smith@gmail.com>
2017-04-17 15:18:29 -0400

    Test Case Results for Demo another test caser

---
+++ tests/pulse/generated/Demo another test caser.spec.js
@@ -0,0 +1,19 @@
+/* globals gauge */
+
+"use strict";
+var assert = require("assert");
+
+/*
+* Demo another test caser
+* 
+* Description should
+*/
+
+/*
+* Preconditions: Have something installed
+* Should write a setup step here
+*/
+
+gauge.step("Step description should Step expected result", function() {
+  assert.equal("Step expected result", "");
+});
