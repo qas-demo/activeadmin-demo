6e340783681c8eed78cd08d509bfd2ac1ec3a3fb
Bobby Smith <bobby.smith@gmail.com>
2017-04-25 11:33:14 -0400

    Test Case Results for Some interesting new test case

---
+++ tests/pulse/generated/Some interesting new test case.spec.js
@@ -0,0 +1,14 @@
+/* globals gauge */
+"use strict";
+var assert = require("assert");
+
+/*
+* Name: Some interesting new test case
+* Description: Some interesting description
+*
+* Preconditions: Some interesting pre condition
+*/
+
+gauge.step("Some description of a step should Some Expected Result", function() {
+  assert.equal("Some Expected Result", "");
+});
