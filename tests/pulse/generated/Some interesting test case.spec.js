ce0e01758ac6894c8e30fd64d5d9aec28da2b83b
Bobby Smith <bobby.smith@gmail.com>
2017-04-25 11:16:21 -0400

    Test Case Results for Some interesting test case

---
+++ tests/pulse/generated/Some interesting test case.spec.js
@@ -0,0 +1,21 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Some interesting test case
+* 
+* 
+*/
+
+/*
+* Preconditions: 
+* Should write a setup step here
+*/
+
+/*describe("Some interesting test case", function(){
+*  it("should Step", function() {
+*    assert("Result" === "");
+*  });
