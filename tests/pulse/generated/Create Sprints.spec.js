773cddc0b0929907e2a4ea9230363823c25af4ad
Bobby Smith <bobby.smith@gmail.com>
2017-05-23 12:50:07 -0400

    Test Case Results for Create Sprints

---
+++ tests/pulse/generated/Create Sprints.spec.js
@@ -0,0 +1,17 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Name: Create Sprints
+* Description: Sprints API endpoint
+*
+* Preconditions: Version 1.2.23
+* Should write a setup step here
+*/
+
+gauge.step("[CREATE] POST /api/v1/sprints should Sprint is created", function() {
+  assert.equal("Sprint is created", "");
+});
