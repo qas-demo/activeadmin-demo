a900492552973d846dea3313033ae6334e719223
Bobby Smith <bobby.smith@gmail.com>
2017-06-05 12:44:25 -0400

    Test Case Results for Create Versions API

---
+++ tests/pulse/generated/Create Versions API.spec.js
@@ -0,0 +1,17 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Create Versions API
+* Some description
+*
+* Preconditions: Some result
+* Should write a setup step here
+*/
+
+gauge.step("[CREATE] POST /api/v1/versions should Some version created", function() {
+  assert.equal("Some version created", "");
+});
