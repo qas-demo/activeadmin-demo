658c635c2c9b83d5a99f0a3f4132802003bfcf5a
Bobby Smith <bobby.smith@gmail.com>
2017-06-08 16:34:37 -0400

    Test Case Results for Test Create Releases Endpoint POST

---
+++ tests/pulse/generated/Test Create Releases Endpoint POST.spec.js
@@ -0,0 +1,18 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Test Create Releases Endpoint POST
+* 
+* 
+*
+* Preconditions: 
+* Should write a setup step here
+*/
+
+gauge.step("[CREATE] POST /api/v1/releases should A release to be created", function() {
+  assert.equal("A release to be created", "");
+});
