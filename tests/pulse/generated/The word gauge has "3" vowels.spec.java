c6e8c498f7e989358484af99b4ca5d9074179679
Bobby Smith <bobby.smith@gmail.com>
2017-06-14 15:22:37 -0400

    Test Case Results for The word gauge has "3" vowels

---
+++ tests/pulse/generated/The word gauge has "3" vowels.spec.java
@@ -0,0 +1,22 @@
+package com.pulsedemo.steps;
+
+import org.junit.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+@Given("^Api v 1.2.3$")
+public void Api v 1.2.3 throws Throwable {
+    throw new PendingException();
+}
+
+@When("^The word "gauge" has 3 vowels$")
+public void The word "gauge" has 3 vowels$ throws Throwable {
+    throw new PendingException();
+}
+
+@Then ("^3$")
+public void 3 throws Throwable {
+    assertEquals("3", "");
+}
