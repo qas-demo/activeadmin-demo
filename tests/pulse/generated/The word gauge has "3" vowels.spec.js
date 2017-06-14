cdd66f1e0ab6eab0ea3c0dc5fd707cd7dfc56b4e
Bobby Smith <bobby.smith@gmail.com>
2017-06-14 15:19:04 -0400

    Test Case Results for The word gauge has "3" vowels

---
+++ tests/pulse/generated/The word gauge has "3" vowels.spec.js
@@ -0,0 +1,17 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* The word gauge has "3" vowels
+* This tests how many vowelrs are in a word
+*
+* Preconditions: Api v 1.2.3
+* Should write a setup step here
+*/
+
+gauge.step("The word "gauge" has 3 vowels should 3", function() {
+  assert.equal("3", "");
+});
