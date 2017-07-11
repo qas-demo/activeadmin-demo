d7b6ba6f40c2c282a050b212145c421037112085
Bobby Smith <bobby.smith@gmail.com>
2017-07-11 14:48:24 -0400

    Test Case Results for Check in should show items count by inventory size

---
+++ tests/pulse/generated/Check in should show items count by inventory size.spec.js
@@ -0,0 +1,21 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Check in should show items count by inventory size
+* 
+* Feature: Check in
+  As a user
+  When I purchase an item
+  It should reduce the inventory count
+*
+* Preconditions: Login Successful
+* Should write a setup step here
+*/
+
+gauge.step("Purchase an item should ", function() {
+  assert.equal("", "");
+});
