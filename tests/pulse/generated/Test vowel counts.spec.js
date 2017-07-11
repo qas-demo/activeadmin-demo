fc729c4fcd6f7637cf2b537892026c997c1e63ee
Bobby Smith <bobby.smith@gmail.com>
2017-07-11 16:25:39 -0400

    Test Case Results for Test vowel counts

---
+++ tests/pulse/generated/Test vowel counts.spec.js
@@ -0,0 +1,18 @@
+/* globals gauge */
+
+"use strict";
+
+var assert = require("assert");
+
+/*
+* Test vowel counts
+* 
+* Test the vowel counts in a word
+*
+* Preconditions: API version for word counts must be 1.2.23
+* Should write a setup step here
+*/
+
+gauge.step("The word "gauge" should have "3" vowels should "3"", function() {
+  assert.equal(""3"", "");
+});
